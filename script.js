a1ea8ed3e5202baa753ce266f5bc169f10ee16a6
adewale-adeyelu <adewaleadeyelu17@gmail.comm>
2024-12-27 15:27:19 +0100

    Aviva

---
+++ script.js
@@ -0,0 +1,9 @@
+// const navbar = document.querySelector('.navbar');
+// window.addEventListener('scroll', () => {
+//     const scrollTop = window.scrollY;
+//     if (scrollTop > 100) {
+//         navbar.style.backgroundcolor = '#fff'
+//     } else {
+//         navbar.style.backgroundcolor = '#ffefdb';
+//     }
+// })
